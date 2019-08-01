7bb3b28e0eab2b1e626bfa5023ecceee6ab1ce71
JhansiSis <nari9177@gmail.com>
2019-08-01 22:31:33 +0530

    committing to take updates

---
+++ RecruiterVille/Areas/SuperUser/Controllers/SuperUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruiterVille.Areas.SuperUser.Controllers
+{
+    public class SuperUser : Controller
+    {
+        // GET: SuperUser/SuperUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
