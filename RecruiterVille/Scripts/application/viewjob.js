829265dfcc4e4c500c65f283ee856aa0be161638
Naveen <Naveen@PC2018>
2019-10-20 19:38:05 +0530

    committing sharing job

--- RecruiterVille/Scripts/application/viewjob.js
+++ RecruiterVille/Scripts/application/viewjob.js
@@ -94,3 +94,19 @@
         }
     });
 }
+
+function shareonfacebook() {
+    var joburl = weburl + 'jobview/' + $('#hiddenselectedjobid').val();
+    window.open('http://www.facebook.com/sharer.php?u=' + $.trim(joburl), 'sharer', 'toolbar=0, status=0, width=626, height=436'); return false;
+}
+
+function shareontwitter() {
+    var joburl = weburl + 'jobview/' + $('#hiddenselectedjobid').val();
+    var jobtitle = $('#labeljobtitle').html();
+    window.open('http://twitter.com/share?text=' + $.trim(jobtitle) + '&url=' + $.trim(joburl), 'sharer', 'toolbar=0, status=0, width=626, height=436,name="twitter:image"'); return false;
+}
+
+function shareonlinkedin() {
+    var joburl = weburl + 'jobview/' + $('#hiddenselectedjobid').val();
+    window.open('https://www.linkedin.com/cws/share?url=' + $.trim(joburl), 'sharer', 'toolbar=0, status=0, width=626, height=436'); return false;
+}
